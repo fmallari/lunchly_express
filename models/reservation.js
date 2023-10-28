f4f00a9922e4352ce859f938af14bec5484a88dc
fmallari <fc.mallari@gmail.com>
2023-10-27 22:53:25 -0700

    set methods for reservations

--- models/reservation.js
+++ models/reservation.js
@@ -82,6 +82,50 @@ get customerId() {
   }
 
 /** find a reservation by ID */
+
+static async get(id) {
+  const result = await db.query(
+    `SELECT id,
+          customer_id AS "customerId",
+          num_guests AS "numGuests",
+          start_at AS "startAt",
+          notes
+        FROM reservations
+        WHERE id = $1`,
+      [id]
+  );
+
+  let reservation = results.row[0];
+
+  if (reservation === undefined) {
+    const err = new Error(`No such reservation: ${id}`);
+    err.status = 400;
+    throw err;
+  }
+
+  return new Reservation(reservation);
+}
+
+/** save reservation */
+
+async save() {
+  if (this.id === undefined) {
+    const result = await db.query(
+      `INSERT INTO reservations (customer_id, num_guests, start_at, notes)
+      VALUES ($1, $2, $3, $4)
+      RETURNING id`,
+      [this.customerId, this.numGuests, this.startAt, this.notes]
+    );
+    this.id = result.rows[0].id;
+  } else {
+    await db.query(
+      `UPDATE reservations SET num_guests=$1, start_at=$2, notes=3
+      WHERE id=$4`,
+    [this.numGuests, this.startAt, this.notes, this.id]
+    );
+  }
+}
+
 }
 
 
